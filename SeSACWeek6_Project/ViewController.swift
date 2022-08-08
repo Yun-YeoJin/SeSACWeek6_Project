6e1d289afe5c5a061b3d5e8afad15cc2e6c19d1c
YunYeoJin <duwls0349@naver.com>
2022-08-08 09:27:30 +0900

    Initial Commit

---
+++ SeSACWeek6_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek6_Project
+//
+//  Created by 윤여진 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
